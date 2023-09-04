db9be0ff87c2f08b62a199d8c2692b0a59202705
KageKing <107029373+KageKing@users.noreply.github.com>
2023-09-04 17:26:44 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,9 +19,9 @@ import { ShippingComponent } from './shipping/shipping.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      {path: 'products/:productId', component:
-    ProductDetailsComponent},
-    { path: 'cart', component: CartComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   declarations: [
