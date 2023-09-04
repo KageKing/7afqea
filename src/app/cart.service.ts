03e11b6473e3a65addd5f0dd732ce47e2f66a095
KageKing <107029373+KageKing@users.noreply.github.com>
2023-09-04 16:05:21 +0200

    4 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
-import { Product } from ' ./products';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './products';
 import { Injectable } from '@angular/core';
 /* . . . */
 @Injectable({
@@ -8,7 +9,7 @@ export class CartService {
 items:Product[] = [];
 /* . . . */
 
-  addTOCart(product : Product) {
+  addToCart(product : Product) {
     this.items.push(product);
   }
 
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
 import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -11,25 +12,23 @@ import { CartService } from '../cart.service';
 export class ProductDetailsComponent implements OnInit {
 
   product: Product | undefined;
- 
-  addToCart(product: Product){
-   this.cartService.addToCart(product);
-   window.alert('Your product has been added to the cart!');
- }
-}
+
   constructor(
     private route: ActivatedRoute, 
     private cartService: CartService
-    ) { }
-  }
+  ) { }
 
   ngOnInit() { // This should be inside the class
     // First get the product id from the current route.
     const routeParams = this.route.snapshot.paramMap;
-    const productIdFromRoute =
-     Number(routeParams.get('productId'));
+    const productIdFromRoute = Number(routeParams.get('productId'));
 
     // Find the product that corresponds with the id provided in the route.
     this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+  addToCart(product: Product){
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
   }
 }
